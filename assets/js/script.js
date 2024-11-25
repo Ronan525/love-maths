1c7798814728f8711e0d044e0249ea80738b2495
Ronan Greenhalgh <RonanGreenhalgh@outlook.com>
2024-11-25 11:10:34 +0000

    Implement answer checking logic and enhance user feedback for game interactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,16 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
 
     runGame ("addition");
-});
+})
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -33,8 +33,24 @@ if (gameType === "addition") {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. AABBOORRT!`;
 }
-function checkAnswer() {
+}
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    }
+    else {
+        alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
 }
 
 /**
@@ -50,11 +66,13 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator}`);
-        throw 'Unimplemented operator ${operator}. AABBOORRT!'; 
-    }
+        if (isCorrect) {
+            incrementScore();
+        } else {
+            incrementWrongAnswer();
+        }
     }
 }
-
 function incrementScore() {
 
 }
